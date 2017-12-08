05356ef4d391b737610f75a563afe53972cc4df2
dov11 <ovo.dan@gmail.com>
2017-12-08 12:27:16 +0100

    add basic grid

--- routes/games.js
+++ routes/games.js
@@ -35,6 +35,44 @@ module.exports = io => {
       newGame.players=[{
         userId: req.account._id
       }]
+      newGame.grid=[
+        {
+        tileIndex: 1,
+        content: 1,
+        },
+        {
+        tileIndex: 2,
+        content: -1,        
+        },
+        {
+        tileIndex: 3,
+        content: 1,
+        },
+        {
+        tileIndex: 4,
+        content: 2,
+        },
+        {
+        tileIndex: 5,
+        content: 2,
+        },
+        {
+        tileIndex: 6,
+        content: 1,
+        },
+        {
+        tileIndex: 7,
+        content: -1,
+        },
+        {
+        tileIndex: 8,
+        content: 1,
+        },
+        {
+        tileIndex: 9,
+        content: 0,
+        },
+    ]
 
       Game.create(newGame)
         .then((game) => {
